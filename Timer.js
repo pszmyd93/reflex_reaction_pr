459728908b75b3910874e9e7b117686caffd0a7a
Pawel Szmyd <lurid.pszmyd@gmail.com>
2021-07-17 02:52:36 +0200

    counting down and pause working

--- Timer.js
+++ Timer.js
@@ -1,17 +1,57 @@
 class Timer {
   constructor(formData) {
-    this.roundDuration = 0;
-    this.breakDuration = 0;
+    this.roundDuration = 60;
+    this.breakDuration = 10;
     this.intervalMin = 0;
     this.intervalMax = 0;
     this.singalsNumber = 0;
-    this.roundsNumber = 0;
+    this.roundsNumber = 3;
     this.signals = [];
-    this.currentTime = { secondsLeft: 0, roundLeft: 0 };
+    this.currentTime = {
+      roundSecondsLeft: this.roundDuration,
+      breakSecondsLeft: this.breakDuration,
+      roundsLeft: this.roundsNumber,
+      isRunning: false,
+      pause: false,
+    };
+    this.interval = 0;
   }
-  startTimer = () => {};
-  pauseTimer = () => {};
-  stopTimer = () => {};
+  startTimer = (e, clock) => {
+    if (this.interval) return;
+    this.currentTime.isRunning = true;
+    this.interval = setInterval(() => {
+      //
+      // if (this.currentTime.roundSecondsLeft == 0) {
+      //   if (this.currentTime.roundsLeft > 0) {
+      //     this.currentTime.roundsLeft = this.currentTime.roundsLeft - 1;
+      //     this.currentTime.pause = !this.currentTime.pause;
+      //   }
+      //   if(this.currentTime.pause == true) {
+      //     this.currentTime.roundSecondsLeft = this.breakDuration;
+      //   } else {
+      //     this.currentTime.roundSecondsLeft = this.roundDuration;
+      //   }
+      // }
+      //
+      this.currentTime.roundSecondsLeft = --this.currentTime.roundSecondsLeft;
+      this.displayTime(clock);
+    }, 1000);
+  };
+  displayTime = clock => {
+    clock.textContent = this.currentTime.roundSecondsLeft;
+  };
+  pauseTimer = () => {
+    this.currentTime.isRunning = false;
+    clearInterval(this.interval);
+    this.interval = 0;
+  };
+  stopTimer = (e, clock) => {
+    this.currentTime.isRunning = false;
+    this.currentTime.roundSecondsLeft = this.roundDuration;
+    this.displayTime(clock);
+    clearInterval(this.interval);
+    this.interval = 0;
+  };
   setData = formData => {};
   playSound = sound => {};
   countDown = () => {};
--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 import Timer from "./Timer.js";
-const clock = document.querySelector(".timer time");
-console.log(clock);
+const TimerLogic = new Timer();
+const timer = document.querySelector(".timer");
+const buttonStart = timer.querySelector(".buttonStart");
+const buttonPause = timer.querySelector(".buttonPause");
+const buttonStop = timer.querySelector(".buttonStop");
+const clock = timer.querySelector(".clock");
+
+buttonStart.addEventListener("click", e => TimerLogic.startTimer(e, clock));
+buttonPause.addEventListener("click", e => TimerLogic.pauseTimer(e));
+buttonStop.addEventListener("click", e => TimerLogic.stopTimer(e, clock));
