873a8e752520d7913e67a5eeed6ab30111fdee9e
Pawel Szmyd <lurid.pszmyd@gmail.com>
2021-08-18 20:04:00 +0200

    timer działa

--- Timer.js
+++ Timer.js
@@ -1,46 +1,79 @@
 class Timer {
-  constructor(formData) {
-    this.roundDuration = 5;
-    this.breakDuration = 3;
-    this.roundsNumber = 2;
-    this.intervalMin = 0;
-    this.intervalMax = 0;
-    this.singalsNumber = 0;
-    this.signals = [];
+  constructor({
+    roundDuration = 5,
+    breakDuration = 3,
+    roundsNumber = 2,
+    intervalMin = 1,
+    intervalMax = 3,
+    signalsNumber = 3,
+  }) {
+    this.roundDuration = roundDuration;
+    this.breakDuration = breakDuration;
+    this.roundsNumber = roundsNumber;
+    this.intervalMin = intervalMin;
+    this.intervalMax = intervalMax;
+    this.signalsNumber = signalsNumber;
     this.currentTime = {
       roundSecondsLeft: this.roundDuration,
-      breakSecondsLeft: this.breakDuration,
       roundsLeft: this.roundsNumber,
       isRunning: false,
       pause: false,
     };
     this.interval = 0;
   }
+  setDataIfFormNotSaved = () => {
+    const form = document.querySelector(".settingsForm");
+    form.signalsNumber.value = this.signalsNumber;
+    form.breakDuration.value = this.breakDuration;
+    form.roundDuration.value = this.roundDuration;
+    form.roundsNumber.value = this.roundsNumber;
+    form.intervalMin.value = this.intervalMin;
+    form.intervalMax.value = this.intervalMax;
+  };
+  toggleButtons = () => {
+    const buttonSave = document.querySelector(".buttonSave");
+    const buttonStart = document.querySelector(".buttonStart");
+    const buttonPause = document.querySelector(".buttonPause");
+    if (this.currentTime.isRunning) {
+      console.dir(buttonSave);
+      buttonSave.disabled = true;
+      buttonStart.disabled = true;
+      buttonPause.disabled = false;
+    }
+    if (!this.currentTime.isRunning) {
+      buttonSave.disabled = false;
+      buttonStart.disabled = false;
+      buttonPause.disabled = true;
+    }
+  };
   startTimer = (e, clock) => {
     if (this.interval) return; // if timer is running return
     this.currentTime.isRunning = true;
-    // this.playSound();
+    this.playSound();
+    this.toggleButtons();
+    this.setDataIfFormNotSaved();
     this.interval = setInterval(() => {
-      console.log(this.interval);
-      //
-      if (this.currentTime.roundSecondsLeft == 0) {
+      if (this.currentTime.roundSecondsLeft == 1) {
+        //round ends, 0 not displayed
         if (this.currentTime.roundsLeft > 0) {
-          this.currentTime.pause = !this.currentTime.pause;
-        } else {
-          this.currentTime.pause = false;
-          this.stopTimer(e, clock);
-          return;
+          if (this.currentTime.roundsLeft == 1 && this.currentTime.pause == false) {
+            this.currentTime.pause = false; //reset pause to be false when starts again
+            this.stopTimer(e, clock);
+            return;
+          } else {
+            this.currentTime.pause = !this.currentTime.pause;
+          }
         }
         if (this.currentTime.pause == true) {
-          this.currentTime.roundSecondsLeft = this.breakDuration;
+          this.currentTime.roundSecondsLeft = this.breakDuration + 1; // +1 because decrements before display
           this.currentTime.roundsLeft = this.currentTime.roundsLeft - 1;
         } else {
-          this.currentTime.roundSecondsLeft = this.roundDuration;
+          this.currentTime.roundSecondsLeft = this.roundDuration + 1;
         }
       }
-      //
+
+      this.currentTime.roundSecondsLeft = this.currentTime.roundSecondsLeft - 1;
       this.displayTime(clock);
-      this.currentTime.roundSecondsLeft = --this.currentTime.roundSecondsLeft;
     }, 1000);
   };
   displayTime = clock => {
@@ -50,6 +83,7 @@ class Timer {
     this.currentTime.isRunning = false;
     clearInterval(this.interval);
     this.interval = 0;
+    this.toggleButtons();
   };
   stopTimer = (e, clock) => {
     this.currentTime.isRunning = false;
@@ -58,19 +92,23 @@ class Timer {
     this.displayTime(clock);
     clearInterval(this.interval);
     this.interval = 0;
+    this.toggleButtons();
   };
-  setData = formData => {};
   playSound = () => {
-    let time = Math.random();
+    const time = Math.random() * 2000;
+    const soundIndex = Math.floor(Math.random() * this.signalsNumber) + 1; // file names starts from 1
+    const sound = new Audio(`sounds/${soundIndex}.mp3`);
+    console.log(`sounds/${soundIndex}.mp3`);
     if (this.currentTime.isRunning == false) return;
     if (this.currentTime.pause == false) {
-      console.log("graj muzyko");
+      // sound.play();
     }
-
     setTimeout(() => {
       this.playSound();
-    }, 1000);
+    }, time);
   };
-  countDown = () => {};
+  formValidate = () => {
+    return this.intervalMin > this.intervalMax;
+  };
 }
 export default Timer;
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import Timer from "./Timer.js";
-const TimerLogic = new Timer();
+const form = document.querySelector(".settingsForm");
+let TimerLogic = new Timer({});
 const timer = document.querySelector(".timer");
 const buttonStart = timer.querySelector(".buttonStart");
 const buttonPause = timer.querySelector(".buttonPause");
@@ -8,6 +9,22 @@ const clock = timer.querySelector(".clock");
 
 TimerLogic.displayTime(clock);
 
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  if (TimerLogic.currentTime.isRunning == true) return;
+  const valid = TimerLogic.formValidate();
+  const data = {
+    signalsNumber: parseInt(form.signalsNumber.value) || 3,
+    roundDuration: parseInt(form.roundDuration.value) || 5,
+    breakDuration: parseInt(form.breakDuration.value) || 3,
+    roundsNumber: parseInt(form.roundsNumber.value) || 2,
+    intervalMin: parseInt(form.intervalMin.value) || 1,
+    intervalMax: parseInt(form.intervalMax.value) || 3,
+  };
+  TimerLogic = new Timer(data);
+  TimerLogic.displayTime(clock);
+  TimerLogic.toggleButtons();
+});
 buttonStart.addEventListener("click", e => TimerLogic.startTimer(e, clock));
 buttonPause.addEventListener("click", e => TimerLogic.pauseTimer(e));
 buttonStop.addEventListener("click", e => TimerLogic.stopTimer(e, clock));
